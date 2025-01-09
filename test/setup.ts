e210d3d9d0d35dc0ae1e194fc9be8d3edafbab6e
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-01-09 09:31:53 -0800

    chore(test): regenerate asset

--- test/setup.ts
+++ test/setup.ts
@@ -3,6 +3,9 @@
  **  bootstrapping the runtime for every test file.
  */
 
+// ? jest-extended will always come from @-xun/symbiote (i.e. transitively)
+// {@symbiote/notInvalid jest-extended}
+
 // ? https://github.com/jest-community/jest-extended#typescript
 import 'jest-extended';
 import 'jest-extended/all';
