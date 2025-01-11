82354130e9ab49b09c5da068b7afbf0eb3e881a2
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-01-11 00:11:12 -0800

    chore(ncurc): revert erroneous change

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -10,7 +10,9 @@ module.exports = {
   install: 'never',
   reject: [
     // ? Reject any super-pinned dependencies (e.g. find-up~5 and execa~7)
-    '*~*'
+    '*~*',
+    // ? Pin the CJS version unless and until debug supports an ESM version
+    'supports-color'
   ]
 };
 
