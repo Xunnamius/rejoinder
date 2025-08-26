e65bdabb0a1086c0e1770a6bb8922ca54556b1b2
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-08-26 00:34:41 -0700

    refactor(packages/cli): fixup help text output

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,5 +1,5 @@
 import { packageUp } from 'package-up';
-import { createDebugLogger } from 'rejoinder';
+import { createDebugLogger, SINGLE_SPACE } from 'rejoinder';
 import { createGithubLogger } from 'rejoinder-github-actions';
 
 import type { ExtendedDebugger, UnextendableInternalDebugger } from '@-xun/debug';
@@ -14,27 +14,39 @@ export default async function command(): Promise<
   const name = getNameFromPackageJson(await packageUp());
 
   return {
+    usage: `Usage: $000 [strings...]\n${SINGLE_SPACE}      rejoin [strings...]\n\n$1.`,
+    description:
+      'Output one or more strings using rejoinder (similar in functionality to the "echo" Linux utility)',
     builder(bf) {
       bf.strict(false);
 
-      bf.example(`$0 hello world!`, '');
-      bf.example(`$0 'hello world!'`, '');
-      bf.example(`$0 --namespace echo:: 'hello world!'`, '');
-      bf.example(`$0 rejoin --name output --use warn Warning! Warning! Warning!`, '');
-      bf.example(`DEBUG='out' $0 --use debug --namespace debug-output HELLO WORLD!`, '');
-      bf.example(`$0 title=Output For Project X::Real output here!`, '');
+      bf.example(`rejoin hello world!`, '');
+      bf.example(`rejoin 'hello world!'`, '');
+      bf.example(`rejoin --namespace echo:: 'hello world!'`, '');
+
+      bf.example(
+        `rejoin rejoin --name output --use warn Warning! Warning! Warning!`,
+        ''
+      );
+
+      bf.example(
+        `DEBUG='out' rejoin --use debug --namespace debug-output HELLO WORLD!`,
+        ''
+      );
+
+      bf.example(`rejoin title=Output For Project X::Real output here!`, '');
 
       return {
         use: {
           choices: ['log', 'message', 'warn', 'error', 'debug'],
           default: 'log',
-          description: 'which logger is used to generate output'
+          description: 'Select which logger is used to generate output'
         },
         namespace: {
           alias: 'name',
           string: true,
-          description: 'the namespace used when generating output',
-          default: name || '>>',
+          description: 'Specify the namespace used when generating output',
+          default: name,
           defaultDescription: `"name" property from nearest package.json file${name ? ` (currently: ${name})` : ''}`
         }
       };
